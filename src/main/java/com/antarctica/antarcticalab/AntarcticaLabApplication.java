3116d423fde734c26e1a4d76454e45e002b77607
housseine <housseine2014tassa@gmail.com>
2019-11-26 10:49:10 +0100

    [init] - init added

---
+++ src/main/java/com/antarctica/antarcticalab/AntarcticaLabApplication.java
@@ -0,0 +1,13 @@
+package com.antarctica.antarcticalab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AntarcticaLabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AntarcticaLabApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/antarctica/antarcticalab/AntarcticaLabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.antarctica.antarcticalab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AntarcticaLabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

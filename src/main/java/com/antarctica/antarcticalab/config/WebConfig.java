10de8a64e286b7444eaf39ea65984958ba112b78
housseine <housseine2014tassa@gmail.com>
2020-03-03 22:34:22 +0100

    [Fix] - added firebase origins

--- src/main/java/com/antarctica/antarcticalab/config/WebConfig.java
+++ src/main/java/com/antarctica/antarcticalab/config/WebConfig.java
@@ -1,22 +1,22 @@
-package com.antarctica.antarcticalab.config;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-@Configuration
-@EnableWebMvc
-public class WebConfig implements WebMvcConfigurer {
-
-	@Override
-	public void addCorsMappings(CorsRegistry corsRegistry) {
-		corsRegistry.addMapping("/**").allowedOrigins("http://localhost:4200").allowedMethods("GET", "PUT", "POST",
-				"PATCH", "DELETE");
-		corsRegistry.addMapping("/**").allowedOrigins("https://antarctica-lab.herokuapp.com/").allowedMethods("GET", "PUT", "POST",
-				"PATCH", "DELETE");
-		corsRegistry.addMapping("/**").allowedOrigins("https://antarctica-lab-web.firebaseapp.com/").allowedMethods("GET", "PUT", "POST",
-				"PATCH", "DELETE");
-	}
-
-}
+//package com.antarctica.antarcticalab.config;
+//
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.servlet.config.annotation.CorsRegistry;
+//import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//
+//@Configuration
+//@EnableWebMvc
+//public class WebConfig implements WebMvcConfigurer {
+//
+//	@Override
+//	public void addCorsMappings(CorsRegistry corsRegistry) {
+//		corsRegistry.addMapping("/**").allowedOrigins("http://localhost:4200").allowedMethods("GET", "PUT", "POST",
+//				"PATCH", "DELETE");
+//		corsRegistry.addMapping("/**").allowedOrigins("https://antarctica-lab.herokuapp.com/").allowedMethods("GET", "PUT", "POST",
+//				"PATCH", "DELETE");
+//		corsRegistry.addMapping("/**").allowedOrigins("https://antarctica-lab-web.firebaseapp.com/").allowedMethods("GET", "PUT", "POST",
+//				"PATCH", "DELETE");
+//	}
+//
+//}

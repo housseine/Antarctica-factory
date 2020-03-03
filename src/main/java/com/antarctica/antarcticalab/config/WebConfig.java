01bef106e33192532d5c0669cb3ba1a46783d2ef
housseine <housseine2014tassa@gmail.com>
2020-03-03 18:53:32 +0100

    [Fix] - added firebase origins

--- src/main/java/com/antarctica/antarcticalab/config/WebConfig.java
+++ src/main/java/com/antarctica/antarcticalab/config/WebConfig.java
@@ -1,33 +1,20 @@
 package com.antarctica.antarcticalab.config;
 
-import javax.sql.DataSource;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import com.zaxxer.hikari.HikariConfig;
-import com.zaxxer.hikari.HikariDataSource;
 
 @Configuration
 @EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
 
-//	@Value("${spring.datasource.url}")
-//	private String dbUrl;
-
 	@Override
 	public void addCorsMappings(CorsRegistry corsRegistry) {
 		corsRegistry.addMapping("/**").allowedOrigins("http://localhost:4200").allowedMethods("GET", "PUT", "POST",
 				"PATCH", "DELETE");
+		corsRegistry.addMapping("/**").allowedOrigins("https://antarctica-lab.herokuapp.com/").allowedMethods("GET", "PUT", "POST",
+				"PATCH", "DELETE");
 	}
-
-//	@Bean
-//	public DataSource dataSource() {
-//		HikariConfig config = new HikariConfig();
-//		config.setJdbcUrl(dbUrl);
-//		return new HikariDataSource(config);
-//	}
 
 }

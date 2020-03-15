c5e840026f9b8cc569273420f584f3f02720ccb5
housseine <housseine2014tassa@gmail.com>
2020-03-15 19:29:23 +0100

    [build] - added proxy

--- src/main/java/com/antarctica/antarcticalab/config/WebConfig.java
+++ src/main/java/com/antarctica/antarcticalab/config/WebConfig.java
@@ -11,8 +11,8 @@ public class WebConfig implements WebMvcConfigurer {
 
 	@Override
 	public void addCorsMappings(CorsRegistry corsRegistry) {
-//		corsRegistry.addMapping("/**").allowedOrigins("http://localhost:4200").allowedMethods("GET", "PUT", "POST",
-//				"PATCH", "DELETE");
+		corsRegistry.addMapping("/**").allowedOrigins("http://localhost:4200").allowedMethods("GET", "PUT", "POST",
+				"PATCH", "DELETE");
 //		corsRegistry.addMapping("/**").allowedOrigins("https://antarctica-lab.herokuapp.com/").allowedMethods("GET", "PUT", "POST",
 //				"PATCH", "DELETE");
 		corsRegistry.addMapping("/**").allowedOrigins("https://antarctica-lab-web.firebaseapp.com").allowedMethods("GET", "PUT", "POST",

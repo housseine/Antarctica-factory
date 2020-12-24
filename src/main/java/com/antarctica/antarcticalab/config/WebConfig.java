017bb0868aa4408df45bfd7aa7630c5c65548966
housseine <housseine2014tassa@gmail.com>
2020-12-24 11:46:05 +0100

    [FIX] - Added CORS

--- src/main/java/com/antarctica/antarcticalab/config/WebConfig.java
+++ src/main/java/com/antarctica/antarcticalab/config/WebConfig.java
@@ -1,36 +1,37 @@
 package com.antarctica.antarcticalab.config;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-//@Configuration
+@Configuration
 @EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
-/*
-	@Override
-	public void addCorsMappings(CorsRegistry corsRegistry) {
-		corsRegistry.addMapping("/**").allowedOrigins("http://localhost:4200").allowedMethods("GET", "PUT", "POST",
-				"PATCH", "DELETE");
-//		corsRegistry.addMapping("/**").allowedOrigins("https://antarctica-lab.herokuapp.com/").allowedMethods("GET", "PUT", "POST",
-//				"PATCH", "DELETE");
-		corsRegistry.addMapping("/**").allowedOrigins("https://antarctica-lab-web.firebaseapp.com").allowedMethods("GET", "PUT", "POST",
-				"PATCH", "DELETE");
-	}*/
-	
-	  private final long MAX_AGE_SECS = 3600;
+	/*
+	 * @Override public void addCorsMappings(CorsRegistry corsRegistry) {
+	 * corsRegistry.addMapping("/**").allowedOrigins("http://localhost:4200").
+	 * allowedMethods("GET", "PUT", "POST", "PATCH", "DELETE"); //
+	 * corsRegistry.addMapping("/**").allowedOrigins(
+	 * "https://antarctica-lab.herokuapp.com/").allowedMethods("GET", "PUT", "POST",
+	 * // "PATCH", "DELETE"); corsRegistry.addMapping("/**").allowedOrigins(
+	 * "https://antarctica-lab-web.firebaseapp.com").allowedMethods("GET", "PUT",
+	 * "POST", "PATCH", "DELETE"); }
+	 */
 
-	    @Override
-	    public void addCorsMappings(CorsRegistry registry) {
-	        registry.addMapping("/**")
-	        .allowedOrigins("http://localhost:4200")
-	        .allowedOrigins("https://antarctica-lab.herokuapp.com/")
-	        .allowedOrigins("https://antarctica-lab-web.firebaseapp.com")
-	        .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
-	        .allowedHeaders("*")
-	        .allowCredentials(true)
-	        .maxAge(MAX_AGE_SECS);
-	    }
-	    
+	private final long MAX_AGE_SECS = 3600;
+
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**")
+				.allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
+				.allowedOrigins("http://localhost:8080")
+				.allowedOrigins("http://localhost:4200")
+				.allowedOrigins("https://antarctica-lab-web.web.app")
+				.allowedOrigins("https://antarctica-lab.herokuapp.com")
+				.allowedHeaders("*")
+				.allowCredentials(true)
+				.maxAge(MAX_AGE_SECS);
+	}
 
 }

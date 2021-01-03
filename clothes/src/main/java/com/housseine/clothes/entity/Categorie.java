e4ce57e2dc0a2dfc4463c940b6845925777653d4
housseine <housseine2014tassa@gmail.com>
2021-01-03 23:38:55 +0100

    [Feat] - added entities

---
+++ clothes/src/main/java/com/housseine/clothes/entity/Categorie.java
@@ -0,0 +1,42 @@
+package com.housseine.clothes.entity;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@JsonAutoDetect
+@Entity
+@Table(name = "CATEGORIE_TABLE")
+public class Categorie {
+	@Id
+	@Column(name = "categorie_id")
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	private String label;
+	@ManyToMany(mappedBy = "categories",fetch = FetchType.LAZY)
+	private Set<Clothes> clothes = new HashSet<>();
+
+	public Categorie(String label, Set<Clothes> clothes) {
+		super();
+		this.label = label;
+		this.clothes = clothes;
+	}
+
+}
---
+++ clothes/src/main/java/com/housseine/clothes/entity/Clothes.java
@@ -0,0 +1,52 @@
+package com.housseine.clothes.entity;
+
+import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@JsonAutoDetect
+@Entity
+@Table(name = "CLOTHES_TABLE")
+public class Clothes {
+	@Id
+	@Column(name = "clothes_id")
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	private String label;
+	private String description;
+	private Date date;
+	@ManyToMany(cascade = { CascadeType.ALL },fetch = FetchType.LAZY)
+	@JoinTable(name = "CLOTHES_CATEGORIE", joinColumns = { @JoinColumn(name = "clothes_id") }, inverseJoinColumns = {
+			@JoinColumn(name = "categorie_id") })
+	Set<Categorie> categories = new HashSet<>();
+
+	public Clothes(String label, String description, Date date, Set<Categorie> categories) {
+		super();
+		this.label = label;
+		this.description = description;
+		this.date = date;
+		this.categories = categories;
+	}
+
+}

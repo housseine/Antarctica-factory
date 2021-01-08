d0b6fb1a7c5b36dbf482c4a3cb05f1fcd9882da8
housseine <housseine2014tassa@gmail.com>
2021-01-08 13:15:20 +0100

    [IT] - Added Database Tests

--- clothes/src/test/java/com/housseine/clothes/infra/bdd/ICategorieRepositoryITest.java
+++ clothes/src/test/java/com/housseine/clothes/infra/bdd/ICategorieRepositoryITest.java
@@ -4,6 +4,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.List;
 
+import org.junit.Before;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,29 +22,51 @@ import com.housseine.clothes.entity.Categorie;
 //@ContextConfiguration
 //@SpringBootTest(classes = ClothesApplication.class)
 @ContextConfiguration(classes = TestConfig.class)
-@EnableJpaRepositories(basePackages = {"com.housseine.*"})
+@EnableJpaRepositories(basePackages = { "com.housseine.*" })
 @EntityScan("com.housseine.clothes.entity")
-
-
 
 @RunWith(SpringRunner.class)
 @DataJpaTest
 class ICategorieRepositoryITest {
 
-    @Autowired
-    private ICategorieRepository iCategorieRepository;
-    
-    @Test
-    public void whenFindByName_thenReturnEmployee() {
-        // given
-        Categorie categorie = new Categorie("women");
-        iCategorieRepository.save(categorie);
+	@Autowired
+	private ICategorieRepository iCategorieRepository;
+	Categorie categorie;
+	List<Categorie> found;
 
-        // when
-        List<Categorie> found = iCategorieRepository.findByLabelContainingIgnoreCase(categorie.getLabel());
+	@Before
+	public void before() {
 
-        // then
-        assertThat(found.get(0).getLabel()).isEqualTo(categorie.getLabel());
-    }
+	}
 
+	@Test
+	public void whenFindByLabelContainingIgnoreCase_thenReturnEmployee() {
+
+		// given
+		categorie = new Categorie("women");
+		iCategorieRepository.save(categorie);
+
+		// when
+		found = iCategorieRepository.findByLabelContainingIgnoreCase(categorie.getLabel());
+
+		// then
+		assertThat(found.get(0).getLabel()).isEqualTo(categorie.getLabel());
+
+	}
+
+	@Test
+	public void whenUpdateCategorie_thenShouldFindIt() {
+		// given
+		categorie = new Categorie("women");
+		iCategorieRepository.save(categorie);
+		Categorie categorie2 = iCategorieRepository.findByLabelContainingIgnoreCase(categorie.getLabel()).get(0);
+		categorie2.setLabel("PullOver");
+		iCategorieRepository.save(categorie2);
+
+		// when
+		found = iCategorieRepository.findByLabelContainingIgnoreCase(categorie2.getLabel());
+
+		// then
+		assertThat(found.get(0).getLabel()).isEqualTo(categorie2.getLabel());
+	}
 }

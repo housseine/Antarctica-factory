d6470b6a1385617935f53a305381d01f069d803f
housseine <housseine2014tassa@gmail.com>
2021-01-09 19:01:05 +0100

    [UT] - added unit test of services

--- clothes/src/test/java/com/housseine/clothes/service/ICategorieServiceTest.java
+++ clothes/src/test/java/com/housseine/clothes/service/ICategorieServiceTest.java
@@ -1,7 +1,14 @@
 package com.housseine.clothes.service;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -32,10 +39,16 @@ class ICategorieServiceTest {
 	@Mock
 	ICategorieDomainRepository categorieDomainRepository;
 
+	Categorie categorie;
+
+	@BeforeEach
+	public void setup() {
+		categorie = new Categorie("Kids");
+	}
+
 	@Test
 	public void shouldAddCategorie() {
 		// Given
-		Categorie categorie = new Categorie("Kids");
 		when(categorieDomainRepository.addCategorie(categorie)).thenReturn(categorie);
 		// when
 		Categorie categorie2 = categorieService.addCategorie(categorie);
@@ -44,4 +57,38 @@ class ICategorieServiceTest {
 		// then
 		assertThat(categorie).isEqualToComparingFieldByField(categorie3);
 	}
+
+	@Test
+	public void shouldUpdateCategorie() {
+		// when
+		categorie.setLabel("New Labem");
+		categorieService.updateCategorie(categorie);
+
+		// then
+		verify(categorieDomainRepository, times(1)).updateCategorie(categorie);
+	}
+
+	@Test
+	public void shouldDeleteCategorie() {
+		// when
+		categorieService.deleteCategorie(1L);
+		// then
+		verify(categorieDomainRepository, times(1)).deleteCategorie(1L);
+	}
+
+	@Test
+	public void shouldGetAllCategories() {
+		// given
+		List<Categorie> categories = new ArrayList<Categorie>();
+		categories.add(new Categorie("kids"));
+		categories.add(new Categorie("Men"));
+		categories.add(new Categorie("Women"));
+		when(categorieDomainRepository.getAllCategories()).thenReturn(categories);
+		// when
+		List<Categorie> categories2 = categorieService.getAllcategories();
+		// then
+		assertThat(categories).isEqualTo(categories2);
+		verify(categorieDomainRepository, times(1)).getAllCategories();
+	}
+
 }
